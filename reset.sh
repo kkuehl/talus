0c3b830f6674848fed3f9f4ae0cdc0b5df981ac3
Kirby Kuehl <kkuehl@gmail.com>
2017-01-09 09:08:56 -0800

    Update reset.sh to support both systemd and upstart.

--- reset.sh
+++ reset.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-sudo stop talus_master
-sudo stop talus_slave
-sudo stop talus_web
-sudo stop talus_amqp
-sudo stop talus_db
+if [[ `systemctl` =~ -\.mount ]]; then
+	sudo systemctl stop talus-master
+	sudo systemctl stop talus-slave
+	sudo systemctl stop docker.talus-web
+	sudo systemctl stop docker.talus-amqp
+	sudo systemctl stop docker.talus-db
+else
+	sudo stop talus-master
+	sudo stop talus-slave
+	sudo stop talus-web
+	sudo stop talus-amqp
+	sudo stop talus-db
+fi
 
 echo sleeping for 10s
 sleep 10
@@ -12,15 +20,26 @@ sleep 10
 sudo kill -KILL $(ps aux | grep slave | grep -v grep | awk '{print $2}')
 sudo kill -KILL $(ps aux | grep master | grep -v grep | awk '{print $2}')
 
-sudo start talus_db
-sudo start talus_amqp
+if [[ `systemctl` =~ -\.mount ]]; then
+	sudo systemctl start docker.talus-db
+	sudo systemctl start docker.talus-amqp
+else
+	sudo start talus-db
+	sudo start talus-amqp
+fi
 
 echo sleeping for db to startup
 sleep 20s
 
-sudo start talus_web
-sudo start talus_master
-sudo start talus_slave
+if [[ `systemctl` =~ -\.mount ]]; then
+	sudo systemctl start docker.talus-web
+	sudo systemctl start talus-master
+	sudo systemctl start talus-slave
+else
+	sudo start talus-web
+	sudo start talus-master
+	sudo start talus-slave
+fi
 
 echo "should be good now..."
 
@@ -30,11 +49,11 @@ echo MASTER
 echo MASTER
 echo MASTER
 echo MASTER
-sudo tail -n 30 /var/log/upstart/talus_master.conf
+sudo tail -n 30 /var/log/upstart/talus-master.log
 
 echo SLAVE
 echo SLAVE
 echo SLAVE
 echo SLAVE
 echo SLAVE
-sudo tail -n 30 /var/log/upstart/talus_slave.conf
+sudo tail -n 30 /var/log/upstart/talus-slave.log
