5bf4e69d8988e42cbac5b96b39fa1f5f2ec414e8
Kirby Kuehl <kkuehl@gmail.com>
2017-06-14 11:51:11 -0700

    update master

--- src/master/lib/vm/__init__.py
+++ src/master/lib/vm/__init__.py
@@ -5,13 +5,12 @@
 Docstring for the VM module
 """
 
-
 class VM(object):
-    """The VM class exposes functionality to manage VMs:
-    * image format conversion
-    * etc"""
+	"""The VM class exposes functionality to manage VMs:
+	* image format conversion
+	* etc"""
 
-    def __init__(self, libvirt_info):
-        """Initialize the VM class (is this really needed?)"""
-        super(VM, self).__init__()
-        self.libvirt_info = libvirt_info
+	def __init__(self, libvirt_info):
+		"""Initialize the VM class (is this really needed?)"""
+		super(VM, self).__init__()
+		self.libvirt_info = libvirt_info
